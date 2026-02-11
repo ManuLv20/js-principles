6f316852a791b5908de3a18ba1551a8b3b7e3465
ManuLv20 <emanueli.goncalves.oliveira@escola.pr.gov.br>
2026-02-11 12:14:17 -0300

    Create produto.js

---
+++ produto.js
@@ -0,0 +1,13 @@
+let nome = "Kaveh"
+let idade = 25;
+let produto = {nome: "Café",
+                valor: "30"};
+let vip = true;
+let quantidade = 15;
+let valorfinal = (produto[valor] * quantidade);
+
+
+if(quantidade > 3){
+   desconto = (valorfinal) - produto[valor]*quantidade*0.1;
+}
+  
