27742e0d061f71d199f6635730854f1cb5777cc0
ManuLv20 <emanueli.goncalves.oliveira@escola.pr.gov.br>
2026-02-12 10:41:54 -0300

    Update continhas.js

--- continhas.js
+++ continhas.js
@@ -1,1 +1,26 @@
-continhas
+//Função afim
+
+let a = 2
+let x = 3
+let b = 4
+let c = 5
+
+let fx = a*x + b
+console.log(`Seu resultado é ${fx}`)
+
+//Função quadrática
+//f(x) = ax^2+ bx + c, sendo a != 0
+
+if (a!=0){
+ fx = a*x**2 + b*x +c
+  console.log(`Seu resultado é ${fx}`)
+}
+
+//Raízes da função quadrática
+
+
+let raiz = b**2 - 4 * a*c
+
+let X1 = (-b + Math.sqrt(raiz))/ 2*a
+let X2 =  (-b - Math.sqrt(raiz))/ 2*a
+console.log(`Seu resultado é X1:${X1} e X2:${X2}, raiz:${raiz}`)
